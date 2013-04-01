263d9e847182bfa2af9072323ea7d5f55a6cf390
Sébastien Crozet <developer@crozet.re>
2013-04-01 12:24:48 +0000

    Fixed argument handling and related lazyness issues for the 8queens demo.

--- Demos/8queens.hs
+++ Demos/8queens.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DoAndIfThenElse #-}
+
 import System.Environment
 import Data.HBDD.ROBDD
 import Data.HBDD.Operations
@@ -7,29 +9,31 @@ import Data.HBDD.ROBDDState
 import Prelude hiding(and,or,not)
 
 usage :: String
-usage = "8queens Demo : hbdd-8queens n [-sat|-count|-satlist]\n\tWhere n is the size of the board"
+usage = "8queens Demo : hbdd-8queens {-sat|-count|-satlist} n\n\tWhere n is the size of the board"
 
 main :: IO ()
 main = do
        args <- getArgs
-       let action (bdd,context) =
-             case head args of
-               "-sat" -> putStrLn $ show $ getSat context bdd
-               "-count" ->  putStrLn $ show $ satCount context bdd
-               "-satlist" -> putStrLn $ show $ getSatList context bdd
-               _ -> putStrLn usage
-       case read $ head $ tail $ args of
-         n -> action $ runState (doit n) mkContext -- FIXME: 1 reine
-         _ -> putStrLn usage
+       if length args /= 2 then
+         putStrLn usage
+       else
+         let n              = read $ head $ tail $ args in
+         let (bdd, context) = runState (doit n) mkContext in
+         case head args of
+           "-sat"     -> putStrLn $ show $ getSat     context bdd
+           "-count"   -> putStrLn $ show $ satCount   context bdd
+           "-satlist" -> putStrLn $ show $ getSatList context bdd
+           _          -> putStrLn usage
 
 -- Checks if the cell (i,j) is forbidden if there is a queen in cell (x,y),
 -- returns True is so.
 inSight :: Int -> (Int,Int) -> ROBDDState (Int,Int)
 inSight n (x,y) =
   do
-  let invalid = (\(i,j) -> ((x,y) /= (i,j)) && ((x ==i) || (y==j) || (x+y)==(i+j) || (x-y) == (i-j)))
+  let invalid = (\(i,j) -> ((x,y) /= (i,j))
+                           && ((x ==i) || (y==j) || (x+y)==(i+j) || (x-y) == (i-j)))
       cannot_exist = (foldr1 (.&.)
-        (map (notC . singletonC) (filter invalid [(i,j) | i <- [1..n], j <- [1..n]])))
+        (map (singletonNotC) (filter invalid [(i,j) | i <- [1..n], j <- [1..n]])))
   (singletonC (x,y)) .=>. cannot_exist
 
 doit :: Int -> ROBDDState (Int,Int)
